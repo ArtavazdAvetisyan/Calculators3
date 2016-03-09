be63a7244da910e69adc43a7ba7ce6dd82c0e09b
ArtavazdAvetisyan <artavazda@hotmail.com>
2016-03-09 22:16:53 +0400

    Added files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+contents = '''
+<!DOCTYPE html>
+<html>
+<head>
+<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+<title>Hello</title>
+</head>
+<body>
+Hello, World!
+</body>
+</html>
+'''
+
+def main():
+	print(contents)
+
+main()
